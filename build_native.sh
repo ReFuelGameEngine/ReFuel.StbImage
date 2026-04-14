432446689c5e06b1fab7c0bca1f6d3365e3b3208
H. Utku Maden <utkumaden@hotmail.com>
2026-04-14 23:30:46 +0300

    Fix typo in build_native.sh

--- build_native.sh
+++ build_native.sh
@@ -9,7 +9,7 @@ DST=ReFuel.StbImage.redis.linux.arm64   ./docker-cross-compiler/sh/build_native.
 DST=ReFuel.StbImage.redis.linux.x64     ./docker-cross-compiler/sh/build_native.sh $PWD linux-x64
 DST=ReFuel.StbImage.redis.osx.arm64     ./docker-cross-compiler/sh/build_native.sh $PWD osx-arm64
 DST=ReFuel.StbImage.redis.osx.x64       ./docker-cross-compiler/sh/build_native.sh $PWD osx-x64
-DST=ReFuel.StbImage.redis.win.x64       ./docker-cross-compiler/sh/build_native.sh $PWD win.x64
-DST=ReFuel.StbImage.redis.win.x86       ./docker-cross-compiler/sh/build_native.sh $PWD win.x86
+DST=ReFuel.StbImage.redis.win.x64       ./docker-cross-compiler/sh/build_native.sh $PWD win-x64
+DST=ReFuel.StbImage.redis.win.x86       ./docker-cross-compiler/sh/build_native.sh $PWD win-x86
 
 dotnet build -c Release
