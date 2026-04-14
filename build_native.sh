1afdc8ffb9e1c36dfdf89f169fdcbf3d4bffc95a
H. Utku Maden <utkumaden@hotmail.com>
2026-04-14 23:27:05 +0300

    Update build scripts.

--- build_native.sh
+++ build_native.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 cd $(dirname "$0")
-./docker-cross-compiler/sh/build_native.sh .
+# Add the local directory as a .NET package source.
+dotnet nuget add source -n ReFuel.StbImage bin
+
+# Build each architecture in its own subfolder.
+DST=ReFuel.StbImage.redis.linux.arm     ./docker-cross-compiler/sh/build_native.sh $PWD linux-arm
+DST=ReFuel.StbImage.redis.linux.arm64   ./docker-cross-compiler/sh/build_native.sh $PWD linux-arm64
+DST=ReFuel.StbImage.redis.linux.x64     ./docker-cross-compiler/sh/build_native.sh $PWD linux-x64
+DST=ReFuel.StbImage.redis.osx.arm64     ./docker-cross-compiler/sh/build_native.sh $PWD osx-arm64
+DST=ReFuel.StbImage.redis.osx.x64       ./docker-cross-compiler/sh/build_native.sh $PWD osx-x64
+DST=ReFuel.StbImage.redis.win.x64       ./docker-cross-compiler/sh/build_native.sh $PWD win.x64
+DST=ReFuel.StbImage.redis.win.x86       ./docker-cross-compiler/sh/build_native.sh $PWD win.x86
+
 dotnet build -c Release
