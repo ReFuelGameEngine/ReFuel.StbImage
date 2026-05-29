2f54c0408a562e88ec208c4bd834917ef5948ac5
H. Utku Maden <utkumaden@hotmail.com>
2026-05-29 19:28:07 +0300

    Create new redistributable packages.

--- build_native.sh
+++ build_native.sh
@@ -4,12 +4,12 @@ cd $(dirname "$0")
 dotnet nuget add source -n ReFuel.StbImage bin
 
 # Build each architecture in its own subfolder.
-DST=ReFuel.StbImage.redis.linux.arm     ./docker-cross-compiler/sh/build_native.sh $PWD linux-arm
-DST=ReFuel.StbImage.redis.linux.arm64   ./docker-cross-compiler/sh/build_native.sh $PWD linux-arm64
-DST=ReFuel.StbImage.redis.linux.x64     ./docker-cross-compiler/sh/build_native.sh $PWD linux-x64
-DST=ReFuel.StbImage.redis.osx.arm64     ./docker-cross-compiler/sh/build_native.sh $PWD osx-arm64
-DST=ReFuel.StbImage.redis.osx.x64       ./docker-cross-compiler/sh/build_native.sh $PWD osx-x64
-DST=ReFuel.StbImage.redis.win.x64       ./docker-cross-compiler/sh/build_native.sh $PWD win-x64
-DST=ReFuel.StbImage.redis.win.x86       ./docker-cross-compiler/sh/build_native.sh $PWD win-x86
+DST=$PWD    ./docker-cross-compiler/sh/build_native.sh $PWD linux-arm64
+DST=$PWD    ./docker-cross-compiler/sh/build_native.sh $PWD linux-arm
+DST=$PWD    ./docker-cross-compiler/sh/build_native.sh $PWD linux-x64
+DST=$PWD    ./docker-cross-compiler/sh/build_native.sh $PWD osx-arm64
+DST=$PWD    ./docker-cross-compiler/sh/build_native.sh $PWD osx-x64
+DST=$PWD    ./docker-cross-compiler/sh/build_native.sh $PWD win-x64
+DST=$PWD    ./docker-cross-compiler/sh/build_native.sh $PWD win-x86
 
 dotnet build -c Release
